5c3904b8c64e1ce2311f9879dd35cfc4cba045c7
Simon Taddiken <simon.taddiken@gmail.com>
2022-01-10 16:18:04 +0100

    Improve xml snapshot customization options

--- readme/RELEASE_NOTES.md
+++ readme/RELEASE_NOTES.md
@@ -1,6 +1,6 @@
 [![Maven Central](https://img.shields.io/static/v1?label=MavenCentral&message=${project.version}&color=blue)](https://search.maven.org/artifact/${project.groupId}/${project.artifactId}/${project.version}/jar) [![JavaDoc](https://img.shields.io/static/v1?label=JavaDoc&message=${project.version}&color=orange)](http://www.javadoc.io/doc/${project.groupId}/${project.artifactId}/${project.version})
 
-* [#11](https://github.com/skuzzle/snapshot-tests/issues/11) Rename `@SnapshotAssertions` to `@EnableSnapshotTests` (**breaking**)
+* Allow to customize the Xml-Unit assertion via `XmlSnapshot.compareUsing(Consumer<CompareAssert>)`
 
 Maven Central coordinates for this release:
 
--- snapshot-tests-jaxb/src/main/java/de/skuzzle/test/snapshots/data/xml/XmlSnapshot.java
+++ snapshot-tests-jaxb/src/main/java/de/skuzzle/test/snapshots/data/xml/XmlSnapshot.java
@@ -1,12 +1,14 @@
 package de.skuzzle.test.snapshots.data.xml;
 
 import java.util.Objects;
+import java.util.function.Consumer;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import org.assertj.core.annotations.Nullable;
+import org.xmlunit.assertj.CompareAssert;
 
 import de.skuzzle.test.snapshots.SnapshotSerializer;
 import de.skuzzle.test.snapshots.StructuralAssertions;
@@ -14,7 +16,8 @@ import de.skuzzle.test.snapshots.StructuredData;
 import de.skuzzle.test.snapshots.StructuredDataBuilder;
 
 /**
- * {@link StructuredData} builder for serializing test results to XML, relying on JAXB.
+ * {@link StructuredData} builder for serializing test results to XML, relying on JAXB and
+ * XML-Unit.
  * <p>
  * You can either use a pre-configured default instance via {@link #xml} or use any of the
  * static factory methods to customize the construction.
@@ -36,7 +39,10 @@ public final class XmlSnapshot implements StructuredDataBuilder {
     // If left null, the JAXBContext will be inferred from the actual test result.
     @Nullable
     private final JAXBContext jaxbContext;
+    // Creates the Marshaller from the JAXBContext
     private MarshallerSupplier marshallerSupplier;
+    // Defines how snapshots are being asserted on using xml-unit
+    private Consumer<CompareAssert> compareAssertConsumer = CompareAssert::areIdentical;
 
     private XmlSnapshot(JAXBContext jaxbContext) {
         this.jaxbContext = jaxbContext;
@@ -51,12 +57,35 @@ public final class XmlSnapshot implements StructuredDataBuilder {
         return new XmlSnapshot(null);
     }
 
+    /**
+     * @param jaxbContext
+     * @return
+     */
     public static XmlSnapshot with(JAXBContext jaxbContext) {
-        return new XmlSnapshot(jaxbContext);
+        return new XmlSnapshot(Objects.requireNonNull(jaxbContext, "jaxbContext must not be null"));
     }
 
+    /**
+     * Supplies the marshaller which will be used to serialize the snapshot to xml.
+     *
+     * @param marshallerSupplier The supplier.
+     * @return This builder instance.
+     */
     public XmlSnapshot withMarshaller(MarshallerSupplier marshallerSupplier) {
         this.marshallerSupplier = Objects.requireNonNull(marshallerSupplier, "marshallerSupplier must not be null");
+        return this;
+    }
+
+    /**
+     * Defines which Xml-Assert assertion method will actually be used. Defaults to
+     * {@link CompareAssert#areIdentical()}.
+     *
+     * @param xmls Consumes the {@link CompareAssert} which compares the actual and
+     *            expected xml.
+     * @return This builder instance.
+     */
+    public XmlSnapshot compareUsing(Consumer<CompareAssert> xmls) {
+        this.compareAssertConsumer = Objects.requireNonNull(xmls, "CompareAssert consumer must not be null");
         return this;
     }
 
@@ -64,10 +93,11 @@ public final class XmlSnapshot implements StructuredDataBuilder {
     public StructuredData build() {
         final SnapshotSerializer snapshotSerializer = JaxbXmlSnapshotSerializer.withExplicitJaxbContext(
                 jaxbContext, marshallerSupplier);
-        final StructuralAssertions structuralAssertions = new XmlUnitStructuralAssertions();
+        final StructuralAssertions structuralAssertions = new XmlUnitStructuralAssertions(compareAssertConsumer);
         return StructuredData.with(snapshotSerializer, structuralAssertions);
     }
 
+    @FunctionalInterface
     static interface MarshallerSupplier {
 
         Marshaller createMarshaller(JAXBContext jaxbContext) throws JAXBException;
