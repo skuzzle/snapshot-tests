c4caf7310e2619222c08e62d749609eec05cc504
Simon Taddiken <simon.taddiken@gmail.com>
2022-03-15 08:40:00 +0100

    Add comment

--- snapshot-tests-core/src/main/java/de/skuzzle/test/snapshots/impl/OrphanPostProcessor.java
+++ snapshot-tests-core/src/main/java/de/skuzzle/test/snapshots/impl/OrphanPostProcessor.java
@@ -10,22 +10,31 @@ import java.util.stream.Stream;
 import de.skuzzle.test.snapshots.impl.OrphanDetectionResult.Result;
 import de.skuzzle.test.snapshots.io.UncheckedIO;
 
+/**
+ * Post processes the results from both the {@link DynamicOrphanedSnapshotsDetector} and
+ * {@link StaticOrphanedSnapshotDetector} according to these rules:
+ * <p>
+ * If both processors produced a result for the same snapshot file and one result was
+ * {@link Result#ACTIVE} it is not considered orphaned. Otherwise it is considered
+ * orphaned if one result was {@link Result#ORPHAN}.
+ *
+ * @author Simon Taddiken
+ */
 class OrphanPostProcessor {
 
     public Stream<OrphanDetectionResult> orphanedOnly(Collection<OrphanDetectionResult> results) {
         return results.stream()
                 .map(result -> resultsFor(result.snapshotFile(), results))
                 .distinct()
-                .map(this::decide)
+                .map(this::decideIsOrphan)
                 .flatMap(Optional::stream);
     }
 
-    private Optional<OrphanDetectionResult> decide(Map<Result, OrphanDetectionResult> map) {
+    private Optional<OrphanDetectionResult> decideIsOrphan(Map<Result, OrphanDetectionResult> map) {
         if (map.containsKey(Result.ACTIVE)) {
             return Optional.empty();
         }
         return Optional.ofNullable(map.get(Result.ORPHAN));
-
     }
 
     private Map<Result, OrphanDetectionResult> resultsFor(Path file, Collection<OrphanDetectionResult> allResults) {
