c31c367322cd19d97a0aacd6ce5ed8db040036a8
Simon Taddiken <simon.taddiken@gmail.com>
2023-01-13 15:04:41 +0100

    Make class public to easily fix javadoc complaints

--- snapshot-tests-junit5/src/main/java/de/skuzzle/test/snapshots/junit5/PlaceHolder.java
+++ snapshot-tests-junit5/src/main/java/de/skuzzle/test/snapshots/junit5/PlaceHolder.java
@@ -1,5 +1,8 @@
 package de.skuzzle.test.snapshots.junit5;
 
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+
 /**
  * Presence of this class will be detected by the -core module in order to print a
  * migration warning. Will be removed with 2.0.0 release.
@@ -8,6 +11,7 @@ package de.skuzzle.test.snapshots.junit5;
  * @since 1.8.0
  */
 @Deprecated(since = "1.8.0", forRemoval = true)
-final class PlaceHolder {
+@API(status = Status.DEPRECATED, since = "1.8.0")
+public final class PlaceHolder {
 
 }
