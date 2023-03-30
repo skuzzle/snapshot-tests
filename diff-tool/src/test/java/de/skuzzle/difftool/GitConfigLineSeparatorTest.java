bc5ec837027933464e9752b0f441cb33529fc198
Simon Taddiken <simon.taddiken@gmail.com>
2023-03-30 09:33:04 +0200

    Fix determining git line ending config values
    Closes #93

--- diff-tool/src/test/java/de/skuzzle/difftool/GitConfigLineSeparatorTest.java
+++ diff-tool/src/test/java/de/skuzzle/difftool/GitConfigLineSeparatorTest.java
@@ -2,6 +2,10 @@ package de.skuzzle.difftool;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -41,6 +45,19 @@ class GitConfigLineSeparatorTest {
         final TestGitConfig git = new TestGitConfig(autocrlf, eol);
         LineSeparator gitLineSeparator = GitLineSeparator.determineGitLineSeparator(git);
         assertThat(gitLineSeparator).isEqualTo(expected);
+    }
+
+    @Test
+    void determineGitConfigValues(@TempDir Path tempDir) {
+        final Path tempGitConfig = tempDir.resolve(".gitconfig");
+        final GitLineSeparator.GitConfig gitConfig = new GitLineSeparator.GitConfig(
+                "-f " + tempGitConfig.toAbsolutePath());
+
+        gitConfig.setAutoCrlf("true");
+        assertThat(gitConfig.autocrlf()).isEqualTo("true");
+
+        gitConfig.setAutoCrlf("input");
+        assertThat(gitConfig.autocrlf()).isEqualTo("input");
     }
 
 }
