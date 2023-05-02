3391bee13d6ec59a3909de7d9418300e28e99a92
Simon Taddiken <simon.taddiken@gmail.com>
2023-05-02 15:40:23 +0200

    Improve doc section about line break normalization

--- snapshot-tests-documentation/src/test/java/de/skuzzle/test/snapshots/snippets/SnapshotTestOptionsTest.java
+++ snapshot-tests-documentation/src/test/java/de/skuzzle/test/snapshots/snippets/SnapshotTestOptionsTest.java
@@ -11,7 +11,7 @@ class SnapshotTestOptionsTest {
 
     @Test
     // tag::normalizeLineEndingsGit[]
-    @SnapshotTestOptions(normalizeLineEndings = SnapshotTestOptions.NormalizeLineEndings.GIT)
+    @SnapshotTestOptions(normalizeLineEndings = SnapshotTestOptions.NormalizeLineEndings.NEVER)
     // end::normalizeLineEndingsGit[]
     void normalizeLineEndingsGit(Snapshot snapshot) {
 
