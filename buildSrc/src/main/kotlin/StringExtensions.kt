b2285d416a8464ea9efe3745aa310d535174249f
Simon Taddiken <simon.taddiken@gmail.com>
2023-05-02 09:25:08 +0200

    Fix execute function to properly split commands

--- buildSrc/src/main/kotlin/StringExtensions.kt
+++ buildSrc/src/main/kotlin/StringExtensions.kt
@@ -1,8 +1,13 @@
+import java.util.*
 import java.util.concurrent.TimeUnit
+import java.util.regex.Matcher
+
 
 fun String.execute(): Unit {
     try {
-        val parts = this.split("\\s".toRegex())
+        require(!this.isEmpty()) { "Empty command" }
+        val parts = splitCommand(this)
+        System.out.println("execute: " + parts)
         val proc = ProcessBuilder(*parts.toTypedArray())
             .redirectOutput(ProcessBuilder.Redirect.PIPE)
             .redirectError(ProcessBuilder.Redirect.PIPE)
@@ -15,4 +20,24 @@ fun String.execute(): Unit {
     } catch (e: Exception) {
         e.printStackTrace()
     }
+}
+
+// from https://stackoverflow.com/a/366532
+fun splitCommand(command: String): List<String> {
+    val matchList: MutableList<String> = ArrayList()
+    val regex = "[^\\s\"']+|\"([^\"]*)\"|'([^']*)'".toRegex().toPattern()
+    val regexMatcher: Matcher = regex.matcher(command)
+    while (regexMatcher.find()) {
+        if (regexMatcher.group(1) != null) {
+            // Add double-quoted string without the quotes
+            matchList.add(regexMatcher.group(1))
+        } else if (regexMatcher.group(2) != null) {
+            // Add single-quoted string without the quotes
+            matchList.add(regexMatcher.group(2))
+        } else {
+            // Add unquoted word
+            matchList.add(regexMatcher.group())
+        }
+    }
+    return matchList
 }
